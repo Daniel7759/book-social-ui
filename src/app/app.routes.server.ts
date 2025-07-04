bbad4e2fa6b95e3f7d4f87abb156470d330c7a0c
Daniel7759 <danieljh7759@gmail.com>
2025-07-03 20:43:11 -0500

    all routes prerender mode, only book/:id client mode

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,11 +2,11 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '',
+    path: '**',
     renderMode: RenderMode.Prerender
   },
   {
-    path: '**',
+    path: 'book/:id',
     renderMode: RenderMode.Client
   }
 ];
